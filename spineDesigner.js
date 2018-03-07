03d11ffe593d51f1f07edaf3da0c5188272d85d2
Heather Zachernuk <35938539+heatherzachernuk@users.noreply.github.com>
2018-03-06 22:36:06 -0800

    .

--- spineDesigner.js
+++ spineDesigner.js
@@ -12,6 +12,7 @@ function addText(){
   config.authorText = authorText;
   saveConfig();
   coverTitle.innerHTML = titleText;
+  coverAuthor.innerHTML = titleText;
   title.innerHTML = titleText;
   author.innerHTML = authorText;
   // updates all the fonts in the list to be the input title
